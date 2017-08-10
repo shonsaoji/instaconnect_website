afab8ba5ae8fcdeb8310c61bbf9ed3e913912430
RiteshSulakhe1 <riteshsulakhe123@gmail.com>
2017-08-10 17:12:16 +0530

    landing page is linked with instaconnect webApp

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -2,6 +2,7 @@ var app = angular.module('instaConnect', []);
 app.controller('homeController', function($scope, $log) {
 	var $ = angular.element
 	baseUrl = 'https://growth-web.herokuapp.com/';
+	// baseUrl = 'http://192.168.2.12:3000/';
 	$scope.scrollToSection = function (id) {
 		var duration;
 		if(id == '#howItWork') {
@@ -14,7 +15,13 @@ app.controller('homeController', function($scope, $log) {
     	}, duration);
 	};
 
-	$scope.gotoLogin = function () {
-		window.location.href = baseUrl+'authentication/signin';
+	$scope.redirectToApp = function (Url) {
+		if(Url == 'login') {
+			window.location.href = baseUrl+'authentication/signin';
+		} else if(Url == 'signUp'){
+			window.location.href = baseUrl+'authentication/signup';
+		} else if(Url == 'code') {
+			window.location.href = baseUrl+'enterCode';
+		}
 	}
 })
