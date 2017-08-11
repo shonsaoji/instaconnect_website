c4cfd039164e24028c2af105eb854acc8922fec4
RiteshSulakhe1 <riteshsulakhe123@gmail.com>
2017-08-11 15:15:50 +0530

    fixed the navbar issue

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -2,7 +2,26 @@ var app = angular.module('instaConnect', []);
 app.controller('homeController', function($scope, $log) {
 	var $ = angular.element
 	baseUrl = 'https://growth-web.herokuapp.com/';
+	$scope.androidDevice = false;
+	$scope.iosDevice = false;
+	$scope.desktopDevice = false;
+
 	// baseUrl = 'http://192.168.2.12:3000/';
+	$scope.checkDeviceType = function () {
+		var isAndroid = /Android/i.test(navigator.userAgent);
+        var isIos = /iPad|iPhone|iPod/i.test(navigator.userAgent);
+
+        if(isAndroid) {
+        	$scope.androidDevice = true;
+        	// $()
+        } else if(isIos) {
+        	$scope.iosDevice = true;
+        } else {
+        	$scope.desktopDevice = true;
+        	console.log('this is the Desktop');
+        }
+
+   	}
 	$scope.scrollToSection = function (id) {
 		var duration;
 		if(id == '#howItWork') {
@@ -19,7 +38,7 @@ app.controller('homeController', function($scope, $log) {
 		if(Url == 'login') {
 			window.location.href = baseUrl+'authentication/signin';
 		} else if(Url == 'signUp'){
-			window.location.href = baseUrl+'authentication/signup';
+			window.location.href = baseUrl;
 		} else if(Url == 'code') {
 			window.location.href = baseUrl+'enterCode';
 		}
